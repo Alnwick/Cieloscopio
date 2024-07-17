55ccf527b1ece0a957131507c7636b09e4b65462
Alnwick <Alnwick0809@outlook.com>
2024-07-17 13:15:12 -0600

    Inicializacion del proyecto.

---
+++ cieloscopio/src/main/java/proyects/cieloscopio/CieloscopioApplication.java
@@ -0,0 +1,13 @@
+package proyects.cieloscopio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CieloscopioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CieloscopioApplication.class, args);
+    }
+
+}
